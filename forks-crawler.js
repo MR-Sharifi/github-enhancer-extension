85d775f23aeff1f64505b5f087fe605e35891cd0
Mohammad-Reza Sharifi <mrsharifi76@gmail.com>
2022-08-07 13:36:17 +0430

    Get Repository Star as a Practice for Retrieving Information

--- forks-crawler.js
+++ forks-crawler.js
@@ -1,5 +1,36 @@
-function getInformation(repositoryUrl) {
-    console.log('information fetched from ' + repositoryUrl);
+class GithubRepository {
+    constructor(repositoryDOM) {
+        this.star = repositoryDOM.getElementById("repo-stars-counter-star").textContent;
+    }
+
+    getInformation() {
+        let information = document.createElement("span");
+        information.className = "flash";
+        information.style.padding = "2px 10px"
+
+        let starInformation = document.createElement("span");
+        starInformation.textContent = this.star;
+
+        information.appendChild(starInformation);
+
+        return information;
+    }
+}
+
+function insertRepositoryInformation(repositoryUrl, repositoryElement) {
+    fetch(repositoryUrl)
+        .then(function (response) {
+            return response.text();
+        })
+        .then(function (html) {
+            let parser = new DOMParser();
+            let DOM = parser.parseFromString(html, "text/html");
+
+            let fetchedRepository = new GithubRepository(DOM);
+
+            repositoryElement.appendChild(fetchedRepository.getInformation());
+            repositoryElement.querySelector("button").remove();
+        });
 }
 
 let allRepositories = document.getElementsByClassName("repo");
@@ -8,9 +39,9 @@ for (let index = 0; index < allRepositories.length; index++) {
         continue;
     }
 
-    let repoElement = allRepositories[index];
+    let repositoryElement = allRepositories[index];
 
-    let repositoryUrl = repoElement.querySelector("a:last-child").href;
+    let repositoryUrl = repositoryElement.querySelector("a:last-child").href;
 
     let button = document.createElement("button");
     button.innerHTML = "Get Information";
@@ -18,16 +49,16 @@ for (let index = 0; index < allRepositories.length; index++) {
     button.hidden = true;
     button.className = "Label Label--success";
     button.onclick = function () {
-        getInformation(repositoryUrl)
+        insertRepositoryInformation(repositoryUrl, repositoryElement);
     };
 
-    repoElement.appendChild(button);
+    repositoryElement.appendChild(button);
 
-    repoElement.onmouseover = function () {
+    repositoryElement.onmouseover = function () {
         button.hidden = false;
     };
 
-    repoElement.onmouseout = function () {
+    repositoryElement.onmouseout = function () {
         button.hidden = true;
     };
 }
